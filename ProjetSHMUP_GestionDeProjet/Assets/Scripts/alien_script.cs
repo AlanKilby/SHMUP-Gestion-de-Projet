8a888d476a3fcea6a65c04b74f592782e4e077f2
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-09-14 17:05:56 +0200

    Saving The Project Test

---
+++ ProjetSHMUP_GestionDeProjet/Assets/Scripts/alien_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alien_script : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            Debug.Log("HIT");
+            Destroy(gameObject);    
+        }
+    }
+}
---
+++ ProjetSHMUP_GestionDeProjet/Assets/Scripts/projectile_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile_script : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Alien"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
